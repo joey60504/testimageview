e032698e8b6bbc048088a9f1b2315b1d73bcd647
hongyee11005 <joey60504@gmail.com>
2022-04-15 16:32:10 +0800

    limit success

--- app/src/main/java/com/example/myapplication/SharedView.kt
+++ app/src/main/java/com/example/myapplication/SharedView.kt
@@ -5,15 +5,12 @@ import android.graphics.Bitmap
 import android.graphics.Matrix
 import android.graphics.RectF
 import android.os.Bundle
-import android.util.Log
 import android.view.MotionEvent
 import android.view.ScaleGestureDetector
 import android.view.ScaleGestureDetector.SimpleOnScaleGestureListener
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivitySharedViewBinding
-import kotlin.math.max
-import kotlin.math.min
 
 
 class SharedView : AppCompatActivity(){
@@ -24,6 +21,8 @@ class SharedView : AppCompatActivity(){
     var drawablewidth   :Float  = 0f
     var drawableheight   :Float  = 0f
     private var scaleFactor = 1.0f
+    var maxscale  = 4.0f
+    var initscale = 1.0f
     var martixValue = FloatArray(9)
     private lateinit var scaleGestureDetector: ScaleGestureDetector
     private lateinit var binding : ActivitySharedViewBinding
@@ -50,39 +49,18 @@ class SharedView : AppCompatActivity(){
         }
         override fun onScale(scaleGestureDetector: ScaleGestureDetector): Boolean {
             scaleFactor = scaleGestureDetector.scaleFactor
-            var scale = getscale()
-            val rectF = RectF()
-            rectF.set(0f, 0f, drawablewidth,drawableheight)
-            matrix.mapRect(rectF)
-
-            var deltaX : Float = 0f
-            var deltaY : Float = 0f
-            if (rectF.width() >= imageViewwidth) {
-                if (rectF.left > 0) {
-                    deltaX = -rectF.left
+            val scale: Float = getscale()
+            if (scale < maxscale && scaleFactor > 1.0f || scale > initscale && scaleFactor < 1.0f) {
+                if (scaleFactor * scale < initscale){
+                    scaleFactor = initscale / scale
                 }
-                if (rectF.right < imageViewwidth) {
-                    deltaX = imageViewwidth - rectF.right
+                if (scaleFactor * scale > maxscale){
+                    scaleFactor = maxscale / scale
                 }
+                matrix.postScale(scaleFactor, scaleFactor, scaleGestureDetector.focusX, scaleGestureDetector.focusY)
+                versioncontrol()
+                binding.imageViewDetail.imageMatrix = matrix
             }
-            if (rectF.height() >= imageViewheight) {
-                if (rectF.top > 0) {
-                    deltaY = -rectF.top
-                }
-                if (rectF.bottom < imageViewheight) {
-                    deltaY = imageViewheight - rectF.bottom
-                }
-            }
-            // 如果宽或高小于屏幕，则让其居中
-            if (rectF.width() < imageViewwidth) {
-                deltaX = imageViewwidth * 0.5f - rectF.right + 0.5f * rectF.width()
-            }
-            if (rectF.height() < imageViewheight) {
-                deltaY = imageViewheight * 0.5f - rectF.bottom + 0.5f * rectF.height()
-            }
-            matrix.postTranslate(deltaX, deltaY)
-            matrix.postScale(scaleFactor,scaleFactor,scaleGestureDetector.focusX,scaleGestureDetector.focusY)
-            binding.imageViewDetail.imageMatrix = matrix
             return true
         }
 
@@ -102,5 +80,48 @@ class SharedView : AppCompatActivity(){
         imageViewheight  =  binding.imageViewDetail.height.toFloat()
         matrix.postTranslate(0f,(imageViewheight-drawableheight)/2)
         binding.imageViewDetail.imageMatrix = matrix
+    }
+    fun versioncontrol(){
+        val rectF = RectF()
+        rectF.set(0f, 0f, drawablewidth,drawableheight)
+        matrix.mapRect(rectF)
+
+        var deltaX  = 0f
+        var deltaY  = 0f
+        if (rectF.width() >= imageViewwidth) {
+            if (rectF.left > 0) {
+                deltaX = -rectF.left
+            }
+            if (rectF.right < imageViewwidth) {
+                deltaX = imageViewwidth - rectF.right
+            }
+        }
+        if (rectF.height() >= imageViewheight) {
+            if (rectF.top > 0) {
+                deltaY = -rectF.top
+            }
+            if (rectF.bottom < imageViewheight) {
+                deltaY = imageViewheight - rectF.bottom
+            }
+        }
+        if (rectF.width() < imageViewwidth) {
+            deltaX = imageViewwidth * 0.5f - rectF.right + 0.5f * rectF.width()
+        }
+        if (rectF.height() < imageViewheight) {
+            deltaY = imageViewheight * 0.5f - rectF.bottom + 0.5f * rectF.height()
+        }
+        matrix.postTranslate(deltaX, deltaY)
+
+        var scale = 1.0f
+        if (rectF.width() > imageViewwidth && rectF.height() <= imageViewheight){
+            scale = imageViewwidth/ rectF.width()
+        }
+        if (rectF.height() > imageViewheight && rectF.width() <= imageViewwidth){
+            scale = imageViewheight / rectF.height()
+        }
+        if (rectF.width() > imageViewwidth && rectF.height() > imageViewheight){
+            scale = Math.min(rectF.width() / imageViewwidth, rectF.height() / imageViewheight)
+        }
+        initscale = scale
     }
 }
