ab51f12dc775ed01355ac1d77ae1416b3f0706f9
hongyee11005 <joey60504@gmail.com>
2022-04-15 15:27:04 +0800

    no limit

--- app/src/main/java/com/example/myapplication/SharedView.kt
+++ app/src/main/java/com/example/myapplication/SharedView.kt
@@ -4,8 +4,6 @@ import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.graphics.Matrix
 import android.graphics.RectF
-import android.opengl.ETC1.getHeight
-import android.opengl.ETC1.getWidth
 import android.os.Bundle
 import android.util.Log
 import android.view.MotionEvent
@@ -20,8 +18,13 @@ import kotlin.math.min
 
 class SharedView : AppCompatActivity(){
 
-    private var scaleFactor = 1.0f
     private var matrix: Matrix = Matrix()
+    var imageViewwidth  :Float  = 0f
+    var imageViewheight :Float  = 0f
+    var drawablewidth   :Float  = 0f
+    var drawableheight   :Float  = 0f
+    private var scaleFactor = 1.0f
+    var martixValue = FloatArray(9)
     private lateinit var scaleGestureDetector: ScaleGestureDetector
     private lateinit var binding : ActivitySharedViewBinding
     @SuppressLint("ClickableViewAccessibility")
@@ -37,31 +40,67 @@ class SharedView : AppCompatActivity(){
         binding.imageViewDetail.setImageBitmap(bmap)
         scaleGestureDetector = ScaleGestureDetector(this,ScaleListener())
     }
-
     override fun onTouchEvent(motionEvent: MotionEvent): Boolean {
         scaleGestureDetector.onTouchEvent(motionEvent)
         return true
     }
-
     private inner class ScaleListener : SimpleOnScaleGestureListener() {
         override fun onScaleBegin(scaleGestureDetector: ScaleGestureDetector): Boolean {
             return super.onScaleBegin(scaleGestureDetector)
         }
         override fun onScale(scaleGestureDetector: ScaleGestureDetector): Boolean {
-            scaleFactor *= scaleGestureDetector.scaleFactor
-            scaleFactor = max(1.0f, min(scaleFactor, 5.0f))
-            Log.d("kkk",scaleFactor.toString())
+            scaleFactor = scaleGestureDetector.scaleFactor
+            var scale = getscale()
+            val rectF = RectF()
+            rectF.set(0f, 0f, drawablewidth,drawableheight)
+            matrix.mapRect(rectF)
+
+            var deltaX : Float = 0f
+            var deltaY : Float = 0f
+            if (rectF.width() >= imageViewwidth) {
+                if (rectF.left > 0) {
+                    deltaX = -rectF.left
+                }
+                if (rectF.right < imageViewwidth) {
+                    deltaX = imageViewwidth - rectF.right
+                }
+            }
+            if (rectF.height() >= imageViewheight) {
+                if (rectF.top > 0) {
+                    deltaY = -rectF.top
+                }
+                if (rectF.bottom < imageViewheight) {
+                    deltaY = imageViewheight - rectF.bottom
+                }
+            }
+            // 如果宽或高小于屏幕，则让其居中
+            if (rectF.width() < imageViewwidth) {
+                deltaX = imageViewwidth * 0.5f - rectF.right + 0.5f * rectF.width()
+            }
+            if (rectF.height() < imageViewheight) {
+                deltaY = imageViewheight * 0.5f - rectF.bottom + 0.5f * rectF.height()
+            }
+            matrix.postTranslate(deltaX, deltaY)
             matrix.postScale(scaleFactor,scaleFactor,scaleGestureDetector.focusX,scaleGestureDetector.focusY)
             binding.imageViewDetail.imageMatrix = matrix
-//            binding.imageViewDetail.pivotX = scaleGestureDetector.focusX - binding.imageViewDetail.x
-//            binding.imageViewDetail.pivotY = scaleGestureDetector.focusY - binding.imageViewDetail.y
-//            binding.imageViewDetail.scaleX = scaleFactor
-//            binding.imageViewDetail.scaleY = scaleFactor
             return true
         }
 
         override fun onScaleEnd(scaleGestureDetector: ScaleGestureDetector) {
             super.onScaleEnd(scaleGestureDetector)
         }
+    }
+    fun getscale() :Float{
+        matrix.getValues(martixValue)
+        return martixValue[Matrix.MSCALE_X]
+    }
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        drawablewidth    =  binding.imageViewDetail.drawable.intrinsicWidth.toFloat()
+        drawableheight   =  binding.imageViewDetail.drawable.intrinsicHeight.toFloat()
+        imageViewwidth   =  binding.imageViewDetail.width.toFloat()
+        imageViewheight  =  binding.imageViewDetail.height.toFloat()
+        matrix.postTranslate(0f,(imageViewheight-drawableheight)/2)
+        binding.imageViewDetail.imageMatrix = matrix
     }
 }
