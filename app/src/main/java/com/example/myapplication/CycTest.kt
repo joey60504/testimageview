064120eafb5d15355b14aa97a182a7f185b03564
Yicheng-1218 <yicheng.881218@gmail.com>
2022-05-05 00:08:09 +0800

    [優化]中繼縮放邏輯

--- app/src/main/java/com/example/myapplication/CycTest.kt
+++ app/src/main/java/com/example/myapplication/CycTest.kt
@@ -3,7 +3,6 @@ package com.example.myapplication
 import android.annotation.SuppressLint
 import android.graphics.Matrix
 import android.graphics.PointF
-import android.graphics.RectF
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -15,7 +14,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.widget.ViewPager2
 import com.example.myapplication.databinding.ActivitySharedViewBinding
-import kotlin.math.abs
 import kotlin.math.pow
 import kotlin.math.sqrt
 
@@ -29,16 +27,13 @@ class CycTest:AppCompatActivity(),CycAdapter.ItemOnTouch {
         const val MODE_ZOOM = 1
     }
 
-    private var startDis = 0.0f
-    private var scale = 0.0f
-    var maxscale  = 4.0f
-    var minscale = 1.0f
-    private val midPoint=PointF()
     private var mode = MODE_NONE
-    private val matrix=Matrix()
+    private var startDis = 0f
     private val currentMatrix=Matrix()
-    var matrixValue = FloatArray(9)
-    private var scaleingMatrix = Matrix()
+    private val matrix=Matrix()
+    private var scale=0f
+    private val midPoint=PointF()
+    private var tempScale:Float? = null
 
 
 
@@ -59,6 +54,7 @@ class CycTest:AppCompatActivity(),CycAdapter.ItemOnTouch {
                 @RequiresApi(Build.VERSION_CODES.Q)
                 override fun onPageScrollStateChanged(state: Int) {
                     view.animationMatrix=currentMatrix
+                    tempScale=null
                     super.onPageScrollStateChanged(state)
                 }
             })
@@ -84,27 +80,35 @@ class CycTest:AppCompatActivity(),CycAdapter.ItemOnTouch {
             MotionEvent.ACTION_MOVE ->{
                 if (mode== MODE_ZOOM){
                     val endDis=distance(event)
-                    scale = endDis / startDis
-                    matrix.getValues(matrixValue)
-                    val prescale = matrixValue[Matrix.MSCALE_X]
-                    if ((prescale < maxscale && scale > minscale) || (prescale > minscale && scale < minscale)) {
-                        if (scale * prescale < minscale) {
-                            scale = minscale / prescale
-                        }
-                        if (scale * prescale > maxscale) {
-                            scale = maxscale / prescale
-                        }
-                        matrix.postScale(scale, scale, midPoint.x, midPoint.y)
-                        versioncontrol(view.height.toFloat(), view.width.toFloat())
-                        view.animationMatrix = matrix
+
+                    scale = endDis/startDis
+                    Log.d("mouse event","scale=${scale}")
+
+
+                    if (tempScale!=null)
+                        scale = if (scale<1f) tempScale!!*scale else tempScale!!+(scale-1)
+
+                    if (scale<1f){
+                        scale=1f
+                    }else if (scale>4f){
+                        scale=4f
                     }
+
+
+
+                    matrix.set(currentMatrix)
+                    matrix.postScale(scale,scale,midPoint.x,midPoint.y)
+                    view.animationMatrix=matrix
+
                 }
             }
+
             MotionEvent.ACTION_POINTER_UP ->{
                 mode = MODE_NONE
             }
             MotionEvent.ACTION_UP->{
                 mode= MODE_NONE
+                tempScale=scale
             }
 
         }
@@ -120,36 +124,5 @@ class CycTest:AppCompatActivity(),CycAdapter.ItemOnTouch {
         val dx=event.getX(1)-event.getX(0)
         val dy=event.getY(1)-event.getY(0)
         return sqrt(dx*dx+dy*dy)
-    }
-    fun versioncontrol(height:Float,width:Float){
-        val rectF = RectF()
-        rectF.set(0f, 0f, width,height)
-        matrix.mapRect(rectF)
-
-        var deltaX  = 0f
-        var deltaY  = 0f
-        if (rectF.width() >= width) {
-            if (rectF.left > 0) {
-                deltaX = -rectF.left
-            }
-            if (rectF.right < width) {
-                deltaX = width - rectF.right
-            }
-        }
-        if (rectF.height() >= height) {
-            if (rectF.top > 0) {
-                deltaY = -rectF.top
-            }
-            if (rectF.bottom < height) {
-                deltaY = height - rectF.bottom
-            }
-        }
-        if (rectF.width() < width) {
-            deltaX = width * 0.5f - rectF.right + 0.5f * rectF.width()
-        }
-        if (rectF.height() < height) {
-            deltaY = height * 0.5f - rectF.bottom + 0.5f * rectF.height()
-        }
-        matrix.postTranslate(deltaX, deltaY)
     }
 }
